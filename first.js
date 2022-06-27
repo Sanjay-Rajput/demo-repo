217ec77ba38d8bedae0bdb24879082bf3bea0e50
Sanjay Rajput <sanjayrajput08002@gmail.com>
2022-06-27 10:35:43 +0530

    demo commit

--- first.js
+++ first.js
@@ -1,4 +1,11 @@
 // add comment
 // add another comment
-console.log("hey, this is my first repo");
-//gcdc
+// console.log("hey, this is my first repo");
+// //gcdc
+var a =5;
+function sum(){
+    var a=8;
+    console.log(a);
+}
+console.log(a);
+sum();
